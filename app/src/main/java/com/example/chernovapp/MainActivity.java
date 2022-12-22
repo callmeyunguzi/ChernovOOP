84ccc2ca070cc57c22d49d2d027bd155a9544ae0
AndrewChernov <Andrewstr33t@yandex.ru>
2022-12-22 16:22:40 +0300

    Полиморфизм

--- app/src/main/java/com/example/chernovapp/MainActivity.java
+++ app/src/main/java/com/example/chernovapp/MainActivity.java
@@ -14,43 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        z++;
-
-        myCat = new Cat(3, "Barsik", "Padre", "red");
-        myCat.talk();
-
-        Cat murzik = new Cat(4, "Murzik", "Fratello", "yellow");
-        murzik.talk();
-
-        Cat glasha = new Cat();
-        glasha.age = 1;
-        glasha.name = "Glasha";
-        glasha.breed = "Don";
-        glasha.color = "brown";
-        glasha.talk();
 
         Cat vaska = new Cat();
         vaska.talk("Meow, meow!");
 
-        Puma puma = new Puma();
-        puma.name = "Leo";
+        Puma bagira = new Puma();
+        bagira.talk();
 
-        puma.breathe();
+        Cat puma = new Cat();
         puma.talk();
-        puma.isAlive = true;
-        Log.i("isAlive", "My name is " + puma.name + " and " + puma.breathe());
-    }
 
-    void method(double x, int y) {
-        int a;
-        a = 1;
-        a = y + 1;
-        double d = x * 2;
-        z = a + y;
-    }
+        Cat murzik = new Lion();
+        murzik.talk();
 
-    void method1() {
-        int a;
-        a = 1;
     }
 }
